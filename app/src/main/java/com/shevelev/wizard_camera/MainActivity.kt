493641b7c27a202dd5e8f8bee92567cc4f07c162
Alex Shevelev <al.e.shevelev@gmail.com>
2020-05-13 12:14:44 +0300

    The navigation bar is half-transparent now

--- app/src/main/java/com/shevelev/wizard_camera/MainActivity.kt
+++ app/src/main/java/com/shevelev/wizard_camera/MainActivity.kt
@@ -5,10 +5,7 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.media.MediaScannerConnection
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.view.TextureView
-import android.view.View
+import android.view.*
 import android.widget.Toast
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
@@ -269,12 +266,14 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
         }
 
     private fun hideSystemUI() {
+        // To make Navigation bar half-transparent
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         window.decorView.systemUiVisibility =
             (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
